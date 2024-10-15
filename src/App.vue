116010c33269f426996938c258e8adaad60e07bd
swafey karanja <kswafey@gmail.com>
2024-10-15 18:42:10 +0300

    learning vue day 2

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,83 @@
 <template>
   <h1>Food</h1>
-  <FoodStuffs
-    food-name="chapati"
-    v-bind:isFavorite="false"
-    food-desc="Apples are a type of fruit that grow on trees."
-  />
-  <FoodStuffs
-    food-name="rice"
-    v-bind:isFavorite="true"
-    food-desc="Pizza has a bread base with tomato sauce."
-  />
-  <FoodStuffs food-name="you" v-bind:isFavorite="false" />
+  <div id="wrapper">
+    <FoodStuffs
+      v-for="x in foods"
+      :key="x.name"
+      :food-name="x.name"
+      :food-desc="x.desc"
+      :is-favorite="x.favorite"
+      @toggle-fave="toggleFavorite"
+      @remove-item="removeFoodItem"
+    />
+  </div>
+  <br />
+  <h3>add items to the list</h3>
+  <input type="text" placeholder="name of the item" v-model="newName" required /><br />
+  <input type="text" placeholder="description of the item" v-model="newDesc" required /><br />
+  <input type="checkbox" name="Favorite" id="isFavorite" v-model="newFave" required /><br />
+  <label for="isFavorite">Favorite</label>
+  <button @click="addItem">Add Item</button>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: 'HIM'
+      foods: [
+        { name: 'Apples', desc: 'Apples are a type of fruit that grow on trees.', favorite: true },
+        {
+          name: 'Pizza',
+          desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.',
+          favorite: false
+        },
+        { name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', favorite: false },
+        { name: 'Fish', desc: 'Fish is an animal that lives in water.', favorite: true },
+        { name: 'Cake', desc: 'Cake is something sweet that tastes good.', favorite: false }
+      ],
+      newName: '',
+      newDesc: '',
+      newFave: false
+    }
+  },
+  methods: {
+    removeFoodItem(foodName) {
+      const index = this.foods.findIndex((food) => food.name === foodName)
+      if (index !== -1) {
+        this.foods.splice(index, 1)
+      }
+    },
+
+    toggleFavorite(foodName) {
+      const foodToToggle = this.foods.find((food) => food.name === foodName)
+      if (foodToToggle) {
+        foodToToggle.favorite = !foodToToggle.favorite
+      }
+    },
+
+    addItem() {
+      const newItem = {
+        name: this.newName,
+        desc: this.newDesc,
+        favorite: this.newFave
+      }
+
+      this.foods.push(newItem), (this.newName = ''), (this.newDesc = ''), (this.newFave = false)
     }
   }
 }
 </script>
 
 <style scoped>
-#app > div {
+#wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+#wrapper > div {
   border: dashed black 1px;
-  display: inline-block;
+  flex-basis: 120px;
   margin: 10px;
   padding: 10px;
   background-color: lightgreen;
-}
-#app > div:hover {
-  cursor: pointer;
 }
 </style>
--- src/components/FoodStuffs.vue
+++ src/components/FoodStuffs.vue
@@ -3,9 +3,11 @@
     <h1>
       {{ foodName }}
     </h1>
+    <div v-show="isFavorite">Fave</div>
     <p>{{ foodDesc }}</p>
     <!-- <span>{{ clicks }}</span> -->
-    <button v-on:click="toggleFave">{{ foodIsFavorite }}</button>
+    <button v-on:click="toggleFave">Favorite</button><br />
+    <button @click="removeItem">Remove</button>
   </div>
 </template>
 
@@ -17,37 +19,19 @@ export default {
       foodIsFavorite: this.isFavorite
     }
   },
-  //   props: ['foodName', 'isFavorite'],
-  props: {
-    foodName: {
-      String,
-      required: true
-    },
-    foodDesc: {
-      String,
-      required: false,
-      default: 'Good food always and forever good food',
-      validator: function (value) {
-        if (20 < value.length && value.length > 80) {
-          return true
-        } else {
-          return false
-        }
-      }
-    },
-    isFavorite: {
-      Boolean,
-      required: false,
-      default: false
-    }
-  },
+  props: ['foodName', 'isFavorite', 'foodDesc'],
+  emits: ['toggle-fave', 'remove-item'],
   methods: {
     countClicks() {
       this.clicks += 1
     },
 
     toggleFave() {
-      this.foodIsFavorite = !this.foodIsFavorite
+      this.$emit('toggle-fave', this.foodName)
+    },
+
+    removeItem() {
+      this.$emit('remove-item', this.foodName)
     }
   }
 }
